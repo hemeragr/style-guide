c225d24d3f09570a4d411a3547765f6618e16fd6
hemeragr <hemeraguerrero@hotmail.com>
2023-05-09 20:28:43 +0200

    first commit

---
+++ src/views/styleguide/Styleguide.jsx
@@ -0,0 +1,23 @@
+import './Styleguide.scss'
+
+
+
+function Styleguide() {
+  return (
+    <>
+      <h1>Colores</h1>
+      <section className='container-colors'>
+        <div className="st-colors color-red">Red</div>
+        <div className="st-colors color-white">White</div>
+        <div className="st-colors color-yellow">Yellow</div>
+        <div className="st-colors color-pink">Pink</div>
+        <div className="st-colors color-blue">Blue</div>
+        <div className="st-colors color-grey">Grey</div>
+        <div className="st-colors color-orange">Orange</div>
+        <div className="st-colors color-purple">Purple</div>
+        <div className="st-colors color-black">Black</div>
+      </section>
+    </>
+  );
+}
+export default Styleguide;
